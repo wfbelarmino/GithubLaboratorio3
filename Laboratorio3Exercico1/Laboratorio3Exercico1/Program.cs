5bb317220bf2efb796598f766b84beade43d3fef
wfbelarmino <92883692+wfbelarmino@users.noreply.github.com>
2021-11-11 14:38:06 -0300

    Add files via upload

---
+++ Laboratorio3Exercico1/Laboratorio3Exercico1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratorio3Exercico1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
